0cd9431311e70ee17744186626b1c9f3181dd1f2
Matt Adewole <matt.adewole@howarddc.com>
2023-08-10 07:48:00 -0400

    Registers custom admin script

--- includes/register-assets.php
+++ includes/register-assets.php
@@ -5,4 +5,14 @@ function ccb_register_assets(){
         'ccb_admin',
         plugins_url('/build/admin/index.css', CCB_PLUGIN_FILE)
     );
+
+    $adminAssets = include(CCB__PLUGIN_DIR . 'build/admin/index.asset.php');
+
+    wp_register_script(
+        'ccb_admin',
+        plugins_url('/build/admin/index.js', CCB_PLUGIN_FILE),
+        $adminAssets['dependencies'],
+        $adminAssets['version'],
+        true
+    );
 }
