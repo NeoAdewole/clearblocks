b2a27bfc7514b86ee3fb794c4364debcd5e8483e
Matt Adewole <9janeo@gmail.com>
2023-08-15 13:19:24 -0500

    Uses clearblocks class method bail_on_activation in Activate

--- src/block-editor/neon-format.js
+++ src/block-editor/neon-format.js
@@ -2,24 +2,31 @@ import './neon.css';
 import { registerFormatType, toggleFormat } from "@wordpress/rich-text";
 import { RichTextToolbarButton } from "@wordpress/block-editor"
 import { __ } from "@wordpress/i18n";
+import { useSelect } from "@wordpress/data";
 
 registerFormatType("clearblocks/neon", {
-    title: __("Neon", "cc-clearblocks"),
-    tagName: "span",
-    edit({ isActive, onChange, value }) {
-        return (
-            <RichTextToolbarButton
-                title={__("Neon", "cc-clearblocks")}
-                icon="superhero"
-                isActive={isActive}
-                onClick ={ () => {
-                    onChange(toggleFormat(value, {
-                            type: "clearblocks/neon",
-                        })
-                    );
-                }}
-            />
-        );
-    },
-    className: 'neon',
+  title: __("Neon", "cc-clearblocks"),
+  tagName: "span",
+  className: 'neon',
+  edit({ isActive, onChange, value }) {
+    const selectedBlock = useSelect(select => select('core/block-editir').getSelectedBlock());
+    return (
+      <>
+        {selectedBlock?.name === "core/paragraph" &&
+          <RichTextToolbarButton
+            title={__("Neon", "cc-clearblocks")}
+            icon="superhero"
+            isActive={isActive}
+            onClick={() => {
+              onChange(toggleFormat(value, {
+                type: "clearblocks/neon",
+              })
+              );
+            }}
+          />
+        }
+      </>
+    );
+  },
+
 });
