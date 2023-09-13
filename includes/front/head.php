6cdf7c058f21e11cd9c3972d3e272e91ea676660
Matt Adewole <9janeo@gmail.com>
2023-09-13 17:33:22 -0500

    Allow override of open graph values on single posts

--- includes/front/head.php
+++ includes/front/head.php
@@ -12,27 +12,26 @@ function ccb_wp_head()
   $description = $options['og_description'];
   $img = $options['og_img'];
   $url = site_url('/');
+  $postID = get_the_id();
 
-  if (is_single()) {
-    $postID = get_the_id();
+  $newTitle = get_post_meta($postID, 'og_title', true);
+  $title = empty($newTitle) ? $title : $newTitle;
 
-    $newTitle = get_post_meta($postID, 'og_title', true);
-    $title = empty($newTitle) ? $title : $newTitle;
+  $newDescription = get_post_meta($postID, 'og_description', true);
+  $description = empty($newDescription) ? $description : $newDescription;
 
-    $newDescription = get_post_meta($postID, 'ogdescription', true);
-    $description = empty($newDescription) ? $description : $newDescription;
-
-    $overrideImage = get_post_meta($postID, 'og_override_image', true);
-    $image = $overrideImage ?
-      get_post_meta($postID, 'og_image', true) :
-      get_the_post_thumbnail($postID, 'openGraph');
-
-    $url = get_permalink($postID);
+  if (is_single($postID)) {
+    // override opengraph for single posts
+    $overrideImage = get_the_post_thumbnail_url($postID, 'openGraph');
+    $title .= ": " . get_the_title($postID);
+    $description .= ". " . get_the_excerpt($postID);
+    $img = $overrideImage ? $overrideImage : $img;
+    $url = get_the_permalink($postID);
   }
 ?>
   <meta property="og:title" content="<?php echo esc_attr($title); ?>" />
   <meta property="og:description" content="<?php echo esc_attr($description); ?>" />
-  <meta property="og:image" content="<?php echo esc_attr($image); ?>" />
+  <meta property="og:image" content="<?php echo esc_attr($img); ?>" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="<?php echo esc_attr($url); ?>" />
 
