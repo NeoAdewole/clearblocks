ba833ec524bf65ad035218e65554bf5fe0670b6f
Matt Adewole <9janeo@gmail.com>
2023-10-02 15:12:25 -0500

    Remove unused functions

--- clear-blocks-plugin.php
+++ clear-blocks-plugin.php
@@ -7,7 +7,7 @@
  * Author:            ClearCut
  * Author URI:        https://clearcutcomms.ca
  * Text Domain:       clearblocks
- * Version:           0.1.1
+ * Version:           0.1.2
  * Requires at least: 6.2
  * Requires PHP:      7.0
  * License:           GPL-2.0-or-later
@@ -27,7 +27,7 @@ if (!function_exists('add_action')) {
 // setup
 
 // Variables
-define('CLEARBLOCKS_VERSION', '0.1.1');
+define('CLEARBLOCKS_VERSION', '0.1.2');
 define('CLEARBLOCKS__MINIMUM_WP_VERSION', '6.2');
 define('CLEARBLOCKS__PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CCB_PLUGIN_FILE', __FILE__);
@@ -41,14 +41,8 @@ foreach ($allFiles as $filename) {
   include_once($filename);
 }
 
-if (is_admin() || (defined('WP_CLI') && WP_CLI)) {
-  require_once(CLEARBLOCKS__PLUGIN_DIR . 'admin/class.clearblocks-admin.php');
-  add_action('init', array('clearblocks_Admin', 'init'));
-}
-// require_once(CLEARBLOCKS__PLUGIN_DIR . 'admin/class.clearblocks-settings-validate.php');
-
 // Hooks
-register_activation_hook(__FILE__, 'ccb_plugin_activation');
+register_activation_hook(__FILE__, array('clearblocks', 'ccb_plugin_activation'));
 register_deactivation_hook(__FILE__, array('clearblocks', 'ccb_plugin_deactivation'));
 
 add_action('init', array('clearblocks', 'init'));
@@ -64,7 +58,7 @@ add_action('save_post_social', 'ccb_save_post_social');
 add_action('after_setup_theme', 'clearblocks_setup_theme');
 add_filter('image_size_names_choose', 'clearblocks_custom_image_sizes');
 add_filter('rest_social_query', 'ccb_rest_social_query', 10, 2);
-add_action('admin_menu', 'ccb_admin_menus');
+add_action('admin_menu', array('Clearblocks_Menu', 'ccb_admin_menus'));
 add_action('admin_post_ccb_save_options', 'ccb_save_options');
 add_action('admin_enqueue_scripts', 'ccb_admin_enqueue');
 add_action('init', 'ccb_register_assets');
@@ -73,35 +67,6 @@ add_action('enqueue_block_editor_assets', 'ccb_enqueue_block_editor_assets');
 add_action('wp_head', 'ccb_wp_head');
 add_action('plugins_loaded', 'ccb_load_php_translations');
 add_action('wp_enqueue_scripts', 'ccb_load_block_translations', 100);
-
-// default plugin options. these are used until the user makes edits
-function clearblocks_options_default()
-{
-
-  return array(
-    'custom_url'     => 'https://disbydem.com/',
-    'custom_title'   => esc_html__('What\'s your DBD\'ers scale?', 'clearblocks'),
-    'custom_style'   => 'disable',
-    'custom_message' => '<p class="custom-message">' . esc_html__('My custom message', 'clearblocks') . '</p>',
-    'custom_footer'  => esc_html__('Special message for users', 'clearblocks'),
-    'custom_toolbar' => false,
-    'custom_scheme'  => 'default',
-    'custom_api_key'  => 'default',
-  );
-}
-
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
-function create_block_clear_blocks_plugin_init()
-{
-  register_block_type(__DIR__ . '/build');
-}
-add_action('init', 'create_block_clear_blocks_plugin_init');
 
 // To Do: Structure folder to allow conditional loading of admin and public files
 // configure src to build scripts and style seperately based on admin/public concerns
--- includes/admin/menus.php
+++ includes/admin/menus.php
@@ -1,21 +1,24 @@
 <?php
-function ccb_admin_menus()
+class Clearblocks_Menu
 {
-  add_menu_page(
-    __('Clearblocks', 'clearblocks'),
-    __('Clearblocks', 'clearblocks'),
-    'edit_theme_options',
-    'clearblocks-plugin-options',
-    'ccb_plugin_options_page',
-    plugins_url('letter-u.svg', CCB_PLUGIN_FILE)
-  );
+  public static function ccb_admin_menus()
+  {
+    add_menu_page(
+      __('Clearblocks', 'clearblocks'),
+      __('Clearblocks', 'clearblocks'),
+      'edit_theme_options',
+      'clearblocks-plugin-options',
+      'ccb_plugin_options_page',
+      plugins_url('letter-u.svg', CCB_PLUGIN_FILE)
+    );
 
-  add_submenu_page(
-    'clearblocks-plugin-options',
-    __('Clearblocks Settings', 'clearblocks'),
-    __('Clearblocks Settings', 'clearblocks'),
-    'edit_theme_options',
-    'clearblocks-plugin-settings',
-    'ccb_plugin_settings_page'
-  );
+    add_submenu_page(
+      'clearblocks-plugin-options',
+      __('Clearblocks Settings', 'clearblocks'),
+      __('Clearblocks Settings', 'clearblocks'),
+      'edit_theme_options',
+      'clearblocks-plugin-settings',
+      'ccb_plugin_settings_page'
+    );
+  }
 }
